e0314abc8e85c2b124faa1f1eecb8df2b02ceb26
Malik Olivier Boussejra <malik@boussejra.com>
2022-02-01 09:57:44 +0900

    Update

--- _nuxt/static/1637395121/en/blog/testing-with-flask/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637395121",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fblog\u002Ftesting-with-flask",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"47e31ddc"}}}}(null));
