c99fa391f8dbc911776a9b0b153f9c164719c9f8
Malik Olivier Boussejra <malik@boussejra.com>
2022-02-18 15:00:37 +0900

    Update

---
+++ _nuxt/static/1645164035/en/blog/tag/web_development/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645164035",layout:"default",error:a,serverRendered:true,routePath:"\u002Fen\u002Fblog\u002Ftag\u002Fweb_development",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"70055090"}}}}(null));
