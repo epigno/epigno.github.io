3907b14e68c01c0b207aeb9585f97bb51bbd043c
Malik Olivier Boussejra <malik@boussejra.com>
2022-06-09 11:05:16 +0900

    Update

--- _nuxt/static/1650522178/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650522178",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"63a2d2b5"}}}}(null,"\u002F"));
